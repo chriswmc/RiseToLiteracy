8c7020f41a9c4a98aae1eeda520c725a7d4fe58a
Chris McCleve <christophermccleve@hotmail.com>
2015-03-20 11:23:20 -0600

    Only first letter caps
    Added caps to apply to only the first letter, while all other letters
    are forced to be lowercase

--- Only letters/ewriter.web/Controllers/HomeController.cs
+++ Only letters/ewriter.web/Controllers/HomeController.cs
@@ -58,8 +58,9 @@ namespace eWriter.Web.Controllers
             hintNode.Location.MakeInfinite();
             hintNode.WordMode = true;
             hintNode.AllowPartialDictionaryTerms = true;
-            string letter = "(a|A|b|B|c|C|d|D|e|E|f|F|g|G|h|H|i|I|j|J|k|K|l|L| " + " m|M|n|N|o|O|p|P|q|Q|r|R|s|S|t|T|u|U|v|V|w|W|x|X|y|Y|z|Z)";
-            hintNode.Factoid = letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter +
+            string letterWithCaps = "(a|A|b|B|c|C|d|D|e|E|f|F|g|G|h|H|i|I|j|J|k|K|l|L| " + " m|M|n|N|o|O|p|P|q|Q|r|R|s|S|t|T|u|U|v|V|w|W|x|X|y|Y|z|Z)";
+            string letter = "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)";
+            hintNode.Factoid = letterWithCaps + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter +
                 letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + letter;
             hintNode.CoerceToFactoid = true;
             hintNode.Name = "Allow Partial Dictionary Terms & Enable WordMode"; 
